26470af50085ca64ed2f556e2c7f6a3f785da613
Ed Baker <edwbaker@gmail.com>
2018-09-24 13:27:08 +0100

    generateNoise returns Wave objects.

--- R/generateNoise.R
+++ R/generateNoise.R
@@ -1,43 +1,13 @@
-#' Windowing Function for Wave Objects
-#'
-#' Seperates a Wave object into windows of a defined length and runs a function on the window section. Windows may overlap, and the function can make use of 'parallel' package for multicore processing.
-#' 
-#' @param wave A Wave object
-#' @param window.length The lag used to create the A-matrix
-#' @param window.overlap A matrix used to code the Duration-Shape pairs
-#' @param max_D The maximum Duration to code
-#' @param FUN If TRUE plots the workings of the coding algorithm
-#' @param ... Additional parameters to FUN
-#' @param cluster A cluser form the 'parallel' package for multicore computation
-#' @import seewave
-#' @import tuneR
-#' @keywords wave
-#' @export
-#' @examples
-#' library(tuneR)
-#' wave <- readWave(system.file("extdata", "1.wav", package="tdsc"))
-#' t <- tdsc(wave)
-
 generateNoise <- function(
-  wave, 
+  input, 
   noise = "white", 
   noiseAdd = FALSE, 
   noiseRatio=0.5, 
   output = "file",
   plot=FALSE) {
   
-  seewave_avail <- requireNamespace("seewave", quietly=TRUE)
-
-  input <- c()
-  for (i in 1:length(files)) {
-    if (file.exists(files[[i]])) {
-      input <- c(input, files[[i]])
-    } else {
-      warning(paste("File:", files[[i]], "could not be found."))
-    }
-  }
-  if (length(input) == 0) {
-    stop("No source files specified.")
+  if (typeof(input) == "S4") {
+    input <- list(input)
   }
   
   noiseComponents <- noise
@@ -45,10 +15,10 @@ generateNoise <- function(
     noise <- paste(noise, collapse="+")
   }
   
-  data <- c()
+  data <- list()
   
   for (i in 1:length(input)) {
-    source <- readWave(input[[i]])
+    source <- mono(input[[i]])
     source <- normalize(source)
     source_d <- duration(source)
     if (plot==TRUE) {
@@ -106,18 +76,9 @@ generateNoise <- function(
           oscillo(ratioNoise)
         }
         
-        row <- c(input[[i]], noise[[j]], noiseRatio[[k]])
+        row <- list(noise=noise[[j]], noiseRatio=noiseRatio[[k]], wave=ratioNoise)
         
-
-        if (output == "file") {
-          filename <- paste0(c(row, ".wav"), collapse="_")
-          savewav(ratioNoise, source@samp.rate, filename=filename)
-        }
-        if (output == "Wave") {
-          row <- c(row, ratioNoise)
-        }
-        
-        data <- rbind(data, row)
+        data <- c(data, list(row))
       }
     }
   }
