5d0e957c7fbd96e0d87bd2177cc8624a87e73ae3
Ed Baker <edwbaker@gmail.com>
2018-10-22 11:28:29 +0100

    ntd can pass vectors to functions.

--- R/ntd.R
+++ R/ntd.R
@@ -6,16 +6,22 @@
 #' @param events Onset of detected events, e.g. from pulseDetction()
 #' @param FUN The function to run
 #' @param normalise If TRUE the output is a probability density
+#' @param argument If "wave" supplies a weave object to the function, if "vector" supplies the left channel as a numeric vector.
 #' @export
 #'
 
-ntd <- function(wave, events, FUN, normalise=FALSE) {
+ntd <- function(wave, events, FUN, normalise=FALSE, argument="wave") {
  N <- length(events)-1
  k <- 1:(length(events)-1) / N
  Fk <- vector(length = length(events)-1)
  for (i in 1:(length(events)-1)) {
    wave_event <- tuneR::Wave(wave@left[events[i]:events[i+1]], samp.rate=wave@samp.rate, bit=wave@bit)
-   Fk[i] <- do.call(FUN, list(wave=wave_event))
+   if (argument=="wave") {
+     Fk[i] <- do.call(FUN, list(wave=wave_event))
+   }
+   if (argument=="vector") {
+     Fk[i] <- do.call(FUN, list(wave_event@left))
+   }
  }
  if (normalise) {
    Fk <- Fk/sum(Fk)
