345ac42d01e090147e3a39b42dd494ee32b3d402
Ed Baker <edwbaker@gmail.com>
2018-10-24 18:44:29 +0100

    Wavelength calcualtor.

---
+++ R/wavelength.R
@@ -0,0 +1,14 @@
+wavelength <- function(frequency, speed=NULL, unit="m") {
+  if (is.null(speed)) {
+    warning("No speed set for waveelngth calculation, relying on value for air (343m/s).")
+    speed <-343
+  }
+  if (unit == "m") {
+    return (speed/frequency)
+  }
+  if (unit == "cm") {
+    return (100 * speed/frequency)
+  }
+  stop("Invalid unit selction: ", unit)
+}
+
