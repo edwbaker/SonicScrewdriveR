e023aac44414c61e6b0929ad20631d63de305e1b
Ed Baker <edwbaker@gmail.com>
2018-10-28 13:56:53 +0000

    Additional validations.

--- R/validationFunctions.R
+++ R/validationFunctions.R
@@ -1,11 +1,55 @@
 validateRH <- function(RH) {
+  if (!is.numeric(RH)) {
+    stop("RH must be numeric")
+  }
   if (RH>100 | RH < 0) {
     stop("Realtive humidity must be between 0 and 100.")
   }
+  return(RH)
 }
 
 validateKelvin <- function(T) {
+  if (!is.numeric(T)) {
+    stop("Kelvin must be numeric")
+  }
   if (T < 0) {
     stop("Temperatures must be above 0K.")
   }
+  return(T)
+}
+
+validateIsWave <- function(wave) {
+  if (typeof(wave) != "S4" | class(wave) != "Wave") {
+    stop("Expecting a Wave object")
+  }
+}
+
+validateFreqIsPossible <- function(f, wave=NULL, samp.rate=NULL) {
+  if (!is.numeric(f)) {
+    stop("Frequency must be numeric.")
+  }
+  if (is.null(wave) & is.null(samp.rate)) {
+    stop("Validation of frequency requires Wave object or samp.rate")
+  }
+  if (!is.null(wave) & !is.null(samp.rate)) {
+    stop("Validation of frequency requires Wave object OR samp.rate")
+  }
+  if (!is.null(wave)) {
+    validateIsWave(wave)
+  }
+  if (!is.null(samp.rate) & !is.numeric(samp.rate)) {
+    stop("samp.rate must be numeric")
+  }
+  if (!is.null(samp.rate)) {
+    if ( f > samp.rate/2) {
+      stop("Frequency is greater than half sample rate.")
+    }
+  }
+  if (!is.null(wave)) {
+    if (f > wave@samp.rate/2) {
+      stop("Frequency is greater than half sample rate.")
+    }
+  }
+
+  return(f)
 }
--- tests/testthat/test-validationFunctions.R
+++ tests/testthat/test-validationFunctions.R
@@ -1,22 +1,34 @@
 context("validationFunctions")
 
-test_that("RH is within limits", {
-  expect_error(validateRH(-0.1))
-})
-test_that("RH is within limits", {
-  expect_error(validateRH(100.1))
-})
-test_that("RH is within limits", {
-  expect_null(validateRH(0))
-})
-test_that("RH is within limits", {
-  expect_null(validateRH(100))
+test_that("RJ is numeric", {
+  expect_error(validateRH("string"), "RH must be numeric")
 })
 
+test_that("RH is within limits", {
+  expect_error(validateRH(-0.1), "Realtive humidity must be between 0 and 100.")
+  expect_error(validateRH(100.1), "Realtive humidity must be between 0 and 100.")
+  expect_silent(validateRH(0))
+  expect_silent(validateRH(100))
+})
 
 test_that("Kelvin is alway non-negative", {
-  expect_error(validateKelvin(-1))
+  expect_error(validateKelvin(-1), "Temperatures must be above 0K.")
+  expect_silent(validateKelvin(0))
 })
-test_that("Kelvin can be 0 (maybe)", {
-  expect_null(validateKelvin(0))
+
+test_that("Kelvin is numeric" , {
+  expect_error(validateKelvin("string"), "Kelvin must be numeric")
+})
+
+test_that("Inputs to validateFreqIsPossible are valid", {
+  expect_error(validateFreqIsPossible("string", samp.rate="string"), "Frequency must be numeric.")
+  expect_error(validateFreqIsPossible(12), "Validation of frequency requires Wave object or samp.rate")
+  expect_error(validateFreqIsPossible(12,wave=tuneR::silence(samp.rate=1000), f=1000), "Validation of frequency requires Wave object OR samp.rate")
+  expect_error(validateFreqIsPossible(12, wave="string"), "Expecting a Wave object")
+  expect_error(validateFreqIsPossible(12, samp.rate="string"), "samp.rate must be numeric")
+})
+
+test_that("validateFreqIsPossible rejects impossible frequencies", {
+  expect_error(validateFreqIsPossible(501, wave=tuneR::sine(freq=10,samp.rate=1000)), "Frequency is greater than half sample rate.")
+  expect_error(validateFreqIsPossible(501, samp.rate=1000), "Frequency is greater than half sample rate.")
 })
