76cbac85429d9bed1bffb54e6e4ec9c5c0cfd624
Ed Baker <edwbaker@gmail.com>
2018-10-28 13:59:00 +0000

    Test plotting is OK in frequencyStats.

--- tests/testthat/test-frequencyStats.R
+++ tests/testthat/test-frequencyStats.R
@@ -12,3 +12,8 @@ test_that("values for the sheep from seewave are consistent", {
   data(sheep_frequencyStats, package="sonicscrewdriver")
   expect_true(identical(frequencyStats(sheep), sheepFrequencyStats))
 })
+
+test_that("plotting is ok", {
+  data(sheep, package="seewave")
+  expect_silent(frequencyStats(sheep, plot=TRUE))
+})
