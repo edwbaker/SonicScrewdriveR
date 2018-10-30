5b8219a1ca192ffc08581f87b50de8636b9b8fae
Ed Baker <edwbaker@gmail.com>
2018-10-30 13:16:19 +0000

    More frequency stats.

--- R/frequencyStats.R
+++ R/frequencyStats.R
@@ -6,12 +6,13 @@
 #' @param wave A Wave object
 #' @param plot IF TRUE displays values
 #' @param warn If TRUE provides warnings when values are not consistent
-#' @importFrom graphics abline plot
+#' @importFrom graphics abline plot title
 #' @export
 #' 
 frequencyStats <- function(
   wave,
   warn = TRUE,
+  lowcut=1,
   plot=FALSE
 ) {
   if (!typeof(wave) == "S4" | !class(wave) == "Wave") {
@@ -21,6 +22,9 @@ frequencyStats <- function(
   
   x <- wave_spec[,1]
   y <- wave_spec[,2]
+  
+  #lowcut
+  y[x < lowcut] <- 0
   
   y<- y^2
   a <- y >= 0.5*max(y)
@@ -36,14 +40,25 @@ frequencyStats <- function(
   regions_s <- s$lengths[which(s$values == TRUE)]
   regions_l_s <- m[which(s$values == TRUE)]
   
+  peak_r <- max(which(regions_l < which(y==max(y))))
+  peak_q <- max(which(regions_l_s < which(y==max(y))))
+  
   longest <- which(regions == max(regions))
   longest_s <- which(regions_s == max(regions_s))
   
-  min_3 <- x[regions_l[longest]]
-  max_3 <- x[regions_l[longest] + regions[longest]]
+  min_3 <- x[regions_l[peak_r]]
+  max_3 <- x[regions_l[peak_r] + regions[peak_r]]
   
-  min_10 <- x[regions_l_s[longest_s]]
-  max_10 <- x[regions_l_s[longest_s] + regions_s[longest_s]]
+  outer_min_3 <- x[]
+  
+  longest_min_3 <- x[regions_l[longest]]
+  longest_max_3 <- x[regions_l[longest] + regions[longest]]
+  
+  min_10 <- x[regions_l_s[peak_q]]
+  max_10 <- x[regions_l_s[peak_q] + regions_s[peak_q]]
+  
+  longest_min_10 <- x[regions_l_s[longest_s]]
+  longest_max_10 <- x[regions_l_s[longest_s] + regions_s[longest_s]]
   
   if(warn) {
     if (min_3 >= max_3) {
@@ -58,18 +73,28 @@ frequencyStats <- function(
     "-3dB" = list(
       "min" = validateFreqIsPossible(min_3, samp.rate=wave@samp.rate),
       "max" = validateFreqIsPossible(max_3, samp.rate=wave@samp.rate),
+      "longest_min" = validateFreqIsPossible(longest_min_3, samp.rate=wave@samp.rate),
+      "longest_max" = validateFreqIsPossible(longest_max_3, samp.rate=wave@samp.rate),
       "peak" = validateFreqIsPossible(x[which(y==max(y))], samp.rate=wave@samp.rate),
       "centre" = validateFreqIsPossible(mean(c(min_3,max_3)), samp.rate=wave@samp.rate),
       "bandwidth" = validateBandwidthIsPossible(max_3 - min_3, samp.rate=wave@samp.rate),
-      "Q" = validateQ(x[which(y==max(y))] / (max_3 - min_3))
+      "Q" = validateQ(x[which(y==max(y))] / (max_3 - min_3)),
+      "outer min" = validateFreqIsPossible(min(x[y > 0.5*max(y)]), samp.rate=wave@samp.rate),
+      "outer max" = validateFreqIsPossible(max(x[y > 0.5*max(y)]), samp.rate=wave@samp.rate),
+      "outer bandwidth" = validateBandwidthIsPossible(max(x[y > 0.5*max(y)]) - min(x[y > 0.5*max(y)]), samp.rate=wave@samp.rate)
     ),
     "-10dB" = list(
       "min" = validateFreqIsPossible(min_10, samp.rate=wave@samp.rate),
       "max" = validateFreqIsPossible(max_10, samp.rate=wave@samp.rate),
+      "longest_min" = validateFreqIsPossible(longest_min_10, samp.rate=wave@samp.rate),
+      "longest_max" = validateFreqIsPossible(longest_max_10, samp.rate=wave@samp.rate),
       "peak" = validateFreqIsPossible(x[which(y==max(y))], samp.rate=wave@samp.rate),
       "centre" = validateFreqIsPossible(mean(c(min_10,max_10)), samp.rate=wave@samp.rate),
       "bandwidth" = validateBandwidthIsPossible(max_10 - min_10, samp.rate=wave@samp.rate),
-      "Q" = validateQ(x[which(y==max(y))] / (max_10 - min_10))
+      "Q" = validateQ(x[which(y==max(y))] / (max_10 - min_10)),
+      "outer min" = validateFreqIsPossible(min(x[y > 0.1*max(y)]), samp.rate=wave@samp.rate),
+      "outer max" = validateFreqIsPossible(max(x[y > 0.1*max(y)]), samp.rate=wave@samp.rate),
+      "outer bandwidth" = validateBandwidthIsPossible(max(x[y > 0.1*max(y)]) - min(x[y > 0.1*max(y)]), samp.rate=wave@samp.rate)
     )
   )
   
@@ -77,6 +102,9 @@ frequencyStats <- function(
     plot(x,y, type="l")
     abline(h=0.5*max(y), v=c(min_3,max_3), col="blue")
     abline(h=0.1*max(y), v=c(min_10,max_10), col="green")
+    abline(v=c(max(x[y > 0.5*max(y)]), min(x[y > 0.5*max(y)])), col="red")
+    abline(v=c(max(x[y > 0.1*max(y)]), min(x[y > 0.1*max(y)])), col="purple")
+    title(main="Bandwidth calculations", xlab="Frequency (kHz)", ylab="Amplitude")
   }
   
   return(data)
