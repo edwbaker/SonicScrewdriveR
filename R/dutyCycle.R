d4f71d10c4bc8899a0b600262f281ebdc6f8f67f
Ed Baker <edwbaker@gmail.com>
2018-11-01 08:57:31 +0000

    Duty cycle as % possible.

--- R/dutyCycle.R
+++ R/dutyCycle.R
@@ -4,15 +4,22 @@
 #' 
 #' @param wave A Wave object 
 #' @param limit Threshold above which to consider the signal
+#' @param output If "unit" the duty cycle will be in the range 0-1. For a percntage use "percent".
 #' @export
 #'
 dutyCycle <- function(
   wave,
-  limit=0.1
+  limit=0.1,
+  output="unit"
 ) {
   wave <- normalize(wave)
   w <- abs(wave@left)
   l <- length(w)
   c <- sum(w > limit*max(w))
-  return(c/l)
+  if (output == "unit") {
+    return(c/l)
+  }
+  if (output == "percent") {
+    return(100*c/l)
+  }
 }
