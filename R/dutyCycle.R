96834aaac30cce1bc9e7965ab6874255a35e9e52
Ed Baker <edwbaker@gmail.com>
2018-11-06 12:20:06 +0000

    Duty cycle.

--- R/dutyCycle.R
+++ R/dutyCycle.R
@@ -12,7 +12,7 @@ dutyCycle <- function(
   limit=0.1,
   output="unit"
 ) {
-  wave <- seewave::normalize(wave)
+  wave <- tuneR::normalize(wave)
   w <- abs(wave@left)
   l <- length(w)
   c <- sum(w > limit*max(w))
