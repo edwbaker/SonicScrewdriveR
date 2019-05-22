c283b1d37f112f5bee5a3121e06d48280890d6d6
Ed Baker <edwbaker@gmail.com>
2019-05-22 15:42:38 +0100

    tSamples function.

--- R/sDuration.R
+++ R/sDuration.R
@@ -1,11 +1,10 @@
 #' Sample duration
 #'
 #' Calculates the time represenetd by n samples in a Wave.
-#' 
+#'
 #' @param n The number of the samples
 #' @param wave A Wave object containing pulses
 #' @param samp.rate Ineteger sampling rate
-#' @param ... Other arguments to pass to pulse detection function
 #' @export
 #'
 #'
@@ -22,4 +21,29 @@ sDuration <- function(
   }
   duration <- n / samp.rate
   return(duration)
+}
+
+#' Samples per time period
+#'
+#' Calculates the number of samples for a given duration of a wave
+#'
+#' @param time The duration in seconds
+#' @param wave A Wave object containing pulses
+#' @param samp.rate Ineteger sampling rate
+#' @export
+#'
+#'
+tSamples <- function(
+  time = 1,
+  wave = NULL,
+  samp.rate = NULL
+){
+  if (is.null(wave) & is.null(samp.rate)){
+    stop("samp.rate or wave must be specified")
+  }
+  if (!is.null(wave)) {
+    samp.rate <- wave@samp.rate
+  }
+  n <- time / sDuration(wave=wave)
+  return(floor(n))
 }
