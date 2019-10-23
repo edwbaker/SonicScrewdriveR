19b981ba01fbed60dbffe715606a709a2a9100a5
Ed Baker <edwbaker@gmail.com>
2019-10-23 18:25:57 +0100

    Package management.

---
+++ R/packageManagement.R
@@ -0,0 +1,20 @@
+package.installed <- function(name, method=NULL, repo=NULL) {
+  if(name %in% rownames(installed.packages()) == TRUE) {return(TRUE)}
+  else {
+    prompt <- askYesNo(paste("Operation requires missing package ",name,". Install now?"))
+    if (prompt == FALSE | is.na(prompt)) {
+      stop("Missing package not installed.")
+    } else {
+      if (is.null(method)) {
+        install.packages(name)
+        return(TRUE)
+      }
+      if (method == "github") {
+        if (package.installed("devtools")) {
+          install_github(paste(repo,name, sep = "/"))
+          return(TRUE)
+        }
+      }
+    }
+  }
+}
