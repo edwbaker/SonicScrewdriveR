222e3d082f7a375d3766b1fb106c1b66f55dc22e
Ed Baker <edwbaker@gmail.com>
2019-10-23 20:51:57 +0100

    Start documentation of spectraMaths.

--- R/spectraMaths.R
+++ R/spectraMaths.R
@@ -1,3 +1,14 @@
+#' Add two spectra from seewave
+#'
+#' This function takes two spectra from seewave (or equivalent) and adds their values. The spectra must have
+#' the same bins.
+#'
+#' @param s1 First spectrum
+#' @param s2 Second spectrum
+#' @param coerceNegative Sets any values below zero to zero, accepted values "input", "output" or "both".
+#' @return A spectrum of s1+s2
+#' @export
+#'
 addSpectra <- function(s1, s2, coerceNegative="no") {
   validateComparableSpectra(s1, s2)
   if(coerceNegative == "input" | coerceNegative == "both") {
@@ -17,6 +28,17 @@ addSpectra <- function(s1, s2, coerceNegative="no") {
   return(s1)
 }
 
+#' Subtract two spectra from seewave
+#'
+#' This function takes two spectra from seewave (or equivalent) and subtracts their values. The spectra must have
+#' the same bins.
+#'
+#' @param s1 First spectrum
+#' @param s2 Second spectrum
+#' @param coerceNegative Sets any values below zero to zero, accepted values "input", "output" or "both".
+#' @return A spectrum of s1 - s2
+#' @export
+#'
 subtractSpectra <- function(s1, s2, coerceNegative="no") {
   validateComparableSpectra(s1, s2)
   if(coerceNegative == "input" | coerceNegative == "both") {
@@ -36,6 +58,14 @@ subtractSpectra <- function(s1, s2, coerceNegative="no") {
   return(s1)
 }
 
+#' Zero spectrum
+#'
+#' This function takes a spectrum from seewave and creates a new zero-valued spectrum with the same structure.
+#'
+#' @param s1 Specrtrum to emulate the structure of.
+#' @return A zero-valued spectrum.
+#' @export
+#'
 zeroSpectrum <- function(s1) {
   validateSpectrum(s1, coerceNegative = TRUE)
   s1[,2] <- rep_len(0, nrow(s1))
