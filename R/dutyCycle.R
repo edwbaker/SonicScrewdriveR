105af5426c90a2c3f9a86013cae50d2b90addb82
Ed Baker <edwbaker@gmail.com>
2019-10-23 19:40:34 +0100

    dutyCycle improvements.

--- R/dutyCycle.R
+++ R/dutyCycle.R
@@ -1,10 +1,15 @@
 #' Calculate the duty cycle of a wave
 #'
 #' Proportion of a wave with signal above the limit
-#' 
-#' @param wave A Wave object 
+#'
+#' @param wave A Wave object
 #' @param limit Threshold above which to consider the signal
-#' @param output If "unit" the duty cycle will be in the range 0-1. For a percntage use "percent".
+#' @param output If "unit" the duty cycle will be in the range 0-1. For a percentage use "percent".
+#' @return A numerical value for the duty cycle between 0 and 1 (or 0 and 100% if percentage output).
+#' @examples
+#' wave <- tuneR::sine(2000)
+#' dc <- dutyCycle(wave)
+#' pc <- dutyCycle(wave, output="percent")
 #' @export
 #'
 dutyCycle <- function(
@@ -17,9 +22,9 @@ dutyCycle <- function(
   l <- length(w)
   c <- sum(w > limit*max(w))
   if (output == "unit") {
-    return(c/l)
+    return(validateDutyCycle(c/l))
   }
   if (output == "percent") {
-    return(100*c/l)
+    return(100*validateDutyCycle(c/l))
   }
 }
