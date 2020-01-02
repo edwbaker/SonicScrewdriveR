c9a8d9e49007196b68d9933dcde279b34c3d1a31
Ed Baker <edwbaker@gmail.com>
2020-01-02 22:08:10 +0000

    Rename function ab_annotations.

--- R/abAnnotations.R
+++ R/abAnnotations.R
@@ -2,14 +2,16 @@
 #'
 #' Search for annotated audio sections on audioBlast (via the ann-o-mate project).
 #'
-#' @param ... Fields and values to filter on.
+#' @param ... Fields and values to filter on. Any field defined by the Ann-o-mate project
+#' may be filtered. The most common is likely to be taxon.
 #' @export
 #' @importFrom utils URLencode
 #' @importFrom jsonlite fromJSON
 #' @return A data frame of matching annotations
 #' @examples
-#' audioblast_annotations(taxon="Gryllotalpa vineae")
-audioblast_annotations <- function(...) {
+#' ab_annotations(taxon="Gryllotalpa vineae")
+#'
+ab_annotations <- function(...) {
   args <- list(...)
   nams <- names(args)
   url <- "https://api.audioblast.org/annotations?"
