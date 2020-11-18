7a5398d688202fd46598fafa912744dd25ff5d36
Ed Baker <edwbaker@gmail.com>
2020-11-18 16:54:54 +0000

    Implement hardRain.

--- R/rainfall_hardRain.R
+++ R/rainfall_hardRain.R
@@ -1,3 +1,7 @@
-rainfall_hardRain <- function(wave) {
-  #TODO
+rainfall_hardRain <- function(wave, threshold="default") {
+  if (threshold == "default") {
+    train.fn <- list.files(system.file("extdata/rain", package = "hardRain"), "\\.wav$", full.names = T)
+    threshold <- hardRain::getThreshold(train.fn, fn = "spec")
+  }
+  ret <- hardRain::classifyRain(wave, threshold)
 }
