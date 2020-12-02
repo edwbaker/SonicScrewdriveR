c334402e014d15a1f332a79bb118990c500329b7
Ed Baker <edwbaker@gmail.com>
2020-12-02 14:13:19 +0000

    More robust handling of BEdoya's method.

--- R/rainfall_bedoya2017.R
+++ R/rainfall_bedoya2017.R
@@ -3,6 +3,10 @@ rainfall_bedoya2017 <- function(wave, Tmean=1e-6, Tsnr=3.5) {
   a <- subset(p, p[,1] > 0.6 & p[,1] < 1.2)
   mean_a <- mean(a[,2])
   std_a <- stats::sd(a[,2])
+  if (nrow(a) == 1) {
+    message("Not enough data to create standard deviation")
+    return(NULL)
+  }
   c <- mean_a/std_a
   if (mean_a > Tmean && c >Tsnr) {
     r <- mean_a
