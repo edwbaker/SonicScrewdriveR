ea2f2ec955a2d1063ad063e09b72a45a41f287bb
Ed Baker <edwbaker@gmail.com>
2021-05-02 16:08:47 +0100

    Remove hardRain support for CRAN reasons.

--- R/rainfall_hardRain.R
+++
@@ -1,7 +0,0 @@
-rainfall_hardRain <- function(wave, threshold="default") {
-  if (threshold == "default") {
-    train.fn <- list.files(system.file("extdata/rain", package = "hardRain"), "\\.wav$", full.names = T)
-    threshold <- hardRain::getThreshold(train.fn, fn = "spec")
-  }
-  ret <- hardRain::classifyRain(wave, threshold)
-}
