0465ab7eb2003a87d13daee2d673c43e5abeda48
Ed Baker <edwbaker@gmail.com>
2021-05-02 15:44:20 +0100

    Preparing for 0.0.4 release

--- NEWS.md
+++ NEWS.md
@@ -1,8 +1,11 @@
 # sonicscrewdriver 0.0.4
 
+- support for reading AudioMoth configuration files
+- support for reading AudioMoth metadata in wave files
 - audioRead function to convert more audio file types to Wave object
 - hardRain package implementation of rainfall detection
-- parseFilename n ow accepts POSIX timestamp as a format
+- parseFilename now accepts POSIX timestamp as a format
+- provides some typical volumes (in decibels)
 
 # sonicscrewdriver 0.0.3
 
--- R/dayPhase.R
+++ R/dayPhase.R
@@ -8,7 +8,7 @@
 #' @param duration Duration of recording
 #' @param lat Latitude of recording device
 #' @param lon Longitude of recording device
-#' @param tz Timezone of recording device when recording was made
+#' @param tz Time-zone of recording device when recording was made
 #' @export
 #' @return Data frame of day phases with absolute timestamps and relative times within file
 #' @examples
@@ -50,7 +50,7 @@ dayPhase <- function(time=Sys.time(), duration=200000, lat=50.1, lon=1.83, tz="U
 #' @param time A time object representing the start time of a recording
 #' @param lat Latitude of recording device
 #' @param lon Longitude of recording device
-#' @param tz Timezone of recording device when recording was made
+#' @param tz Time-zone of recording device when recording was made
 #'
 #' @importFrom suncalc getSunlightTimes
 dayPhases <- function(time, lat, lon, tz) {
@@ -100,7 +100,7 @@ dayPhases <- function(time, lat, lon, tz) {
 #' @param plot If true plots the data, default FALSE
 #' @param lat Latitude of recording device
 #' @param lon Longitude of recording device
-#' @param tz Timezone of recording device when recording was made
+#' @param tz Time-zone of recording device when recording was made
 #' @export
 #' @importFrom suncalc getSunlightTimes getMoonIllumination getMoonTimes
 #' @importFrom hms as_hms
