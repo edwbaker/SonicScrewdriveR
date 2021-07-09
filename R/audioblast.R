b0a2481430bd997843576f74b1fd9acb9b2ccf98
Ed Baker <edwbaker@gmail.com>
2021-07-09 16:08:02 +0100

    Allow specifying audioblast endpoint.

--- R/audioblast.R
+++ R/audioblast.R
@@ -3,7 +3,8 @@
 #' Search for recordings or analyses on audioBlast.
 #'
 #' @param type One of data, analysis, standalone.
-#' @param name of data or analysis source.
+#' @param name Name of data or analysis source.
+#' @param endpoint Optionally specify endpoint of an audioBlast module.
 #' @param ... Fields and values to filter on. Any field defined by audioBLAST.
 #' @export
 #' @importFrom utils URLencode
@@ -14,10 +15,14 @@
 #' audioblast("data", "recordings", taxon="Gryllotalpa vineae")
 #' }
 #'
-audioblast <- function(type, name, ...) {
+audioblast <- function(type, name, endpoint=NULL, ...) {
   args <- list(...)
   nams <- names(args)
-  url <- paste0("https://api.audioblast.org/",type,"/",name,"/?agent=sonicscrewdriver")
+  url <- paste0("https://api.audioblast.org/",type,"/",name,"/")
+  if (!is.null(endpoint)) {
+    url <- paste0(url, endpoint, "/")
+  }
+  url <- paste0(url, "?agent=sonicscrewdriver")
   for (i in 1:length(args)) {
     url <- paste0(url, "&", nams[[i]], "=", args[[i]])
   }
