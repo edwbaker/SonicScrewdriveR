18f1cc539efc38b0f0c866d80a28bb703734e4ee
Ed Baker <edwbaker@gmail.com>
2021-08-14 08:57:13 +0100

    Start arena plot

--- R/arena_plot.R
+++ R/arena_plot.R
@@ -1,7 +1,54 @@
-arena.plot <- function(arena) {
-  plot(arena@members[[2]]@x, arena@members[[2]]@y, type="l", xlim=c(-50,10), ylim=c(0,10))
-  points(0,0)
-  points(10,10)
-  print(arena@members[[2]]@x)
-  print(length(arena@members[[2]]@x))
+arena.plot <- function(arena, type="basic", t=NULL, ...) {
+  if (type=="basic") {
+    arena.plot.basic(arena, ...)
+  }
+  if (type=="movement") {
+    arena.plot.movement(arena, ...)
+  }
+  if (type=="vectorfield") {
+    arena.plot.vectorfield(arena, ...)
+  }
+}
+
+arena.plot.basic <- function(arena, t, ...) {
+  #ToDo: plot is too specific
+  plot(arena@members[[2]]@x,
+       arena@members[[2]]@y,
+       type="l",
+       xlim=c(-50,10),
+       ylim=c(0,10))
+}
+
+arena.plot.movement <- function(a) {
+  m <- max(a_extract(res, "x"), a_extract(res, "y"))
+  plot(0,
+       0,
+       xlim=c(-m,m),
+       ylim=c(-m,m),
+       xlab="",
+       ylab="",
+       asp=1,
+       type="n"
+  )
+  points(a_extract(res, "x")[1,],
+         a_extract(res, "y")[1,]
+         )
+  arrows(a_extract(res, "x")[1,],
+         a_extract(res, "y")[1,],
+         a_extract(res, "x")[a@max.time,],
+         a_extract(res, "y")[a@max.time,],
+         angle=10,
+         length=.1
+  )
+
+  for (i in 1:length(a@members)) {
+    lines(a_extract(a, "x")[,i],
+          a_extract(res, "y")[,i],
+          col="grey"
+    )
+  }
+}
+
+arena.plot.vectorfield <- function(arena, ...){
+
 }
