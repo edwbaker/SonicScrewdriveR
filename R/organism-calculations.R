cde4a1008410c4eb3b7254fadc7bff1f64f8670a
Ed Baker <edwbaker@gmail.com>
2021-08-19 09:22:09 +0100

    Prepare for 3D

--- R/organism-calculations.R
+++ R/organism-calculations.R
@@ -1,6 +1,6 @@
 o_distance <- function(o1, o2) {
-  s <- (o2@position[2] - o1@position[2])^2 + (o2@position[1] - o1@position[1])^2
-  return(sqrt(s))
+  s <- sqrt(sum((o2@position - o1@position)^2))
+  return(s)
 }
 
 o_angle <- function(o1, o2) {
