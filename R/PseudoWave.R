5101c860627a5f32c3d00fe7833cce856cc90e71
Ed Baker <edwbaker@gmail.com>
2022-03-27 20:56:10 +0100

    PseudoWave docs pass check().

--- R/PseudoWave.R
+++ R/PseudoWave.R
@@ -48,7 +48,6 @@ depseudoNoise <- function(type, n, stereo, samp.rate, bit, pcm) {
   return(noise(kind=type, duration=n, stereo=stereo, samp.rate=samp.rate, bit=bit, pcm=pcm))
 }
 
-#' @importFrom methods callGeneric validObject
 setMethod("Arith", signature(e1 = "Wave", e2 = "PseudoWave"),
   function(e1, e2){
     e2 <- depseduoWave(e2, n=length(e1@left), stereo=e1@stereo, samp.rate=e1@samp.rate, bit=e1@bit, pcm=e1@pcm)
@@ -60,8 +59,6 @@ setMethod("Arith", signature(e1 = "Wave", e2 = "PseudoWave"),
       e1@right <- callGeneric(e1@right, e2@right)
     e1
 })
-
-
 
 setMethod("Arith", signature(e1 = "PseudoWave", e2 = "Wave"),
   function(e1, e2){
@@ -75,6 +72,13 @@ setMethod("Arith", signature(e1 = "PseudoWave", e2 = "Wave"),
     e1
 })
 
+
+#' PseudoWave scalar manipulation
+#'
+#' @docType methods
+#' @param e1 Input 1
+#' @param e2 Input 2
+#' @importFrom methods callGeneric validObject
 setMethod("*", signature(e1 = "PseudoWave", e2 = "numeric"),
   function(e1, e2){
     validObject(e1)
@@ -82,54 +86,67 @@ setMethod("*", signature(e1 = "PseudoWave", e2 = "numeric"),
     e1
 })
 
+#' PseudoWave scalar division
+#'
+#' @docType methods
+#' @param e1 Input 1
+#' @param e2 Input 2
 setMethod("/", signature(e1 = "PseudoWave", e2 = "numeric"),
-  function(e1, e2){
-    validObject(e1)
-    e1@scale <- e1@scale/e2
-    e1
-})
+          function(e1, e2){
+            validObject(e1)
+            e1@scale <- e1@scale/e2
+            e1
+          })
 
+#' PseudoWave scalar addition
+#'
+#' @docType methods
+#' @param e1 Input 1
+#' @param e2 Input 2
 setMethod("+", signature(e1 = "PseudoWave", e2 = "numeric"),
-  function(e1, e2){
-    validObject(e1)
-    e1@offset <- e1@offset+e2
-    e1
-})
+          function(e1, e2){
+            validObject(e1)
+            e1@offset <- e1@offset+e2
+            e1
+          })
 
+#' PseudoWave scalar subtraction
+#'
+#' @docType methods
+#' @param e1 Input 1
+#' @param e2 Input 2
 setMethod("-", signature(e1 = "PseudoWave", e2 = "numeric"),
-  function(e1, e2){
-    validObject(e1)
-    e1@offset <- e1@offset-e2
-    e1
-})
+          function(e1, e2){
+            validObject(e1)
+            e1@offset <- e1@offset-e2
+            e1
+          })
 
+#' Numeric multiplication by PseudoWave
+#'
+#' @docType methods
+#' @param e1 Input 1
+#' @param e2 Input 2
 setMethod("*", signature(e1 = "numeric", e2 = "PseudoWave"),
-  function(e1, e2){
-    validObject(e2)
-    e2@scale <- e2@scale*e1
-    return(e2)
-  }
+          function(e1, e2){
+            validObject(e2)
+            e2@scale <- e2@scale*e1
+            return(e2)
+          }
 )
 
-setMethod("/", signature(e1 = "numeric", e2 = "PseudoWave"),
-  function(e1, e2){
-    validObject(e2)
-    e2@scale <- e2@scale/e1
-    return(e2)
-  }
-)
-
+#' Numeric addition by PseudoWave
+#'
+#' @docType methods
+#' @param e1 Input 1
+#' @param e2 Input 2
 setMethod("+", signature(e1 = "numeric", e2 = "PseudoWave"),
-  function(e1, e2){
-    validObject(e2)
-    e2@offset <- e2@offset+e1
-    return(e2)
-  }
+          function(e1, e2){
+            validObject(e2)
+            e2@offset <- e2@offset+e1
+            return(e2)
+          }
 )
 
-setMethod("-", signature(e1 = "PseudoWave", e2 = "missing"),
-function(e1){
-  validObject(e1)
-  e1@scale <- e1@scale*-1
-  e1
-})
+
+
