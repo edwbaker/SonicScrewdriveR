8f38a14f9fb789304e57020f248f501496b242e1
Ed Baker <edwbaker@gmail.com>
2022-10-06 13:13:12 +0100

    Fix respeaker 6 channels

--- R/respeaker6.R
+++ R/respeaker6.R
@@ -15,7 +15,7 @@
 readRespeaker6 <- function(filename, from=1, to=Inf, units="samples", header=FALSE) {
   w <- readWave(filename, from, to, units, header, toWaveMC=TRUE)
   w@.Data[which(is.na(w@.Data))] <- 0
-  w@.Data <- w@.Data[,c(1,2,5,6,7,8)]
+  w@.Data <- w@.Data[,1:6]
   colnames(w@.Data) <- c("FL", "FR", "FC", "BL", "BR", "BC")
   return(w)
 }
