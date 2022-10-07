7a2567720c4cf024b1999a44ad6f72db8c619ca1
Ed Baker <edwbaker@gmail.com>
2022-10-07 15:30:19 +0100

    corWaveMC using lapply and parLapply

--- R/corWaveMC.R
+++ R/corWaveMC.R
@@ -4,28 +4,32 @@
 #' events between the channels of a WaveMC object
 #' @param wave A WaveMC object
 #' @param times One or more times of events to correlate
-#' @param window Width of the window to correlate in seconds (centered on times)
+#' @param window Width of the window to correlate in seconds (centred on times)
 #' @param temp Air temperature in Celsius
+#' @param cluster A cluster for parallel execution
 #' @return List of corenv lists for events, and a list of the time differences between channels
 #' @export
-#' @importFrom seewave corenv
-corWaveMC <- function(wave, times, window, temp=25) {
+corWaveMC <- function(wave, times, window, temp=25, cluster=NULL) {
   validateIsWaveMC(wave)
 
+  outtimes <- vector(mode="list", length=length(times))
   for (i in 1:length(times)) {
     offsets <- vector(mode="list", length=wave@dim[2])
     delays <- vector(mode="numeric", length=wave@dim[2])
-    #Todo: convert to parallel apply
-    for (c in 1:wave@dim[2]) {
-      start <- times[i] - window/2
-      end <- times[i] + window/2
-      offsets[[c]] <- corenv(cutw(wave[,1],from=start,to=end, output="Wave"), cutw(wave[,c],from=start,to=end, output="Wave"), plot=F)
-      delays[c] <- offsets[[c]]$t
+    start <- times[i]-window/2
+    end <- times[i]+window/2
+    if (is.null(cluster)) {
+      offsets <- lapply(1:wave@dim[2], corWaveMCchannel, wave=wave, from=start, to=end)
+    } else {
+      offsets <- parLapply(cluster, 1:wave@dim[2], corWaveMCchannel, wave=wave, from=start, to=end)
     }
+    outtimes[[i]] <- offsets
   }
-  data <- list(
-    "raw" = offsets,
-    "times" = delays
-  )
-  return(data)
+  return(outtimes)
+}
+
+#' @importFrom seewave corenv
+corWaveMCchannel <- function (channel2, wave, from, to){
+  ret <- corenv(cutw(wave[,1],from=from,to=to, output="Wave"), cutw(wave[,channel2],from=from,to=to, output="Wave"),plot=F)
+  return(ret)
 }
