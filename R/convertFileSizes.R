f63776e7dd3d798dda6ab5bbef373cb8b0f57bbf
Ed Baker <929684+edwbaker@users.noreply.github.com>
2022-10-20 15:33:41 +0100

    Convert more byte values

--- R/convertFileSizes.R
+++ R/convertFileSizes.R
@@ -3,7 +3,7 @@
 #' Converts time measurements into seconds
 #'
 #' @param S The value to convert
-#' @param input The unit  to convert, allowed values are "bits"
+#' @param input The unit  to convert, allowed values are "bits", "kB", "MB", "GB"
 #' @export
 #' @return The numeric value in seconds
 #'
@@ -13,6 +13,15 @@ convert2bytes <- function(S, input="bits") {
   }
   if (input =="bytes") {
     return(S)
+  }
+  if (input == "kB") {
+    return(S*1e+3)
+  }
+  if (input == "MB") {
+    return(S*1e+6)
+  }
+  if (input == "GB") {
+    return(S*1e+9)
   }
   stop(paste("Unknown input to convert2bytes:",input))
 }
