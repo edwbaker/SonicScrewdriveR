2fc655595c0aa52b1058e3f1a561c09cc78276a3
Ed Baker <edwbaker@gmail.com>
2023-02-05 14:04:01 +0000

    allChannels() to work with soundecology

--- R/allChannels.R
+++ R/allChannels.R
@@ -6,36 +6,62 @@
 #' @param w A Wave or WaveMC object
 #' @param FUN Function to apply to the wave.
 #' @param cl Optionally a cluster for parallel calculation.
-#' @param channel.param Optional. Name of the channel paramater to FUN.
+#' @param channel.param Optional. Name of the channel parameter to FUN.
+#' @param output.FUN Optional. Function that processes the output of FUN.
+#'   The "channels_se" function provides standard functionality for the
+#'   soundecology package.
 #' @param ... Optional. Additional paramaters to pass to FUN.
 #' @return A list of outputs.
 #' @export
-allChannels <- function(w, FUN, cl=NULL, channel.param="channel",  ...) {
+allChannels <- function(w, FUN, cl=NULL, channel.param="channel",  output.FUN=NULL, ...) {
   if (is(w, "Wave")) {
     if (w@stereo == FALSE) {
       ret <- doChannel(1, w, channel.param=channel.param, FUN, ...)
       return(ret)
     } else {
       if (is.null(cl)) {
-        ret <- sapply(1:2, doChannel, w=w,  channel.param=channel.param, FUN, ...)
+        ret <- sapply(1:2, doChannel, w=w,  channel.param=channel.param, output.FUN=output.FUN, FUN, ...)
       } else {
-        ret <- parSapply(cl, 1:2, doChannel, w=w, channel.param=channel.param, FUN, ...)
+        ret <- parSapply(cl, 1:2, doChannel, w=w, channel.param=channel.param, output.FUN=output.FUN, FUN, ...)
       }
       return(ret)
     }
   } else if (is(w, "WaveMC")) {
     if (is.null(cl)) {
-      ret <- sapply(1:w@dim[2], doChannel, w=w,  channel.param=channel.param, FUN, ...)
+      ret <- sapply(1:w@dim[2], doChannel, w=w,  channel.param=channel.param, output.FUN=output.FUN, FUN, ...)
     } else {
-      ret <- parSapply(cl, 1:w@dim[2], doChannel, w=w, channel.param=channel.param, FUN, ...)
+      ret <- parSapply(cl, 1:w@dim[2], doChannel, w=w, channel.param=channel.param, output.FUN=output.FUN, FUN, ...)
     }
     return(ret)
   }
 }
 
-doChannel <- function(channel, w, channel.param, FUN, ...) {
+#' @importFrom tuneR channel
+doChannel <- function(channel, w, channel.param, output.FUN, FUN, ...) {
+  if (is.null(channel.param)) {
+    if (is(w, "Wave")) {
+      if (channel == 1) {
+        w <- channel(w, "left")
+      } else if (channel == 2) {
+        w <- channel(w, "right")
+      }
+    } else if (is(w, "WaveMC")) {
+      w <- w[,channel]
+    }
+  }
   l <- list(FUN, w, ...)
-  l[as.character(channel.param)] <- channel
+  if (!is.null(channel.param)) {
+    l[as.character(channel.param)] <- channel
+  }
   ret <- eval(as.call(l))
+
+  if (!is.null(output.FUN)) {
+    ret <- do.call(output.FUN, ret)
+  }
   return(ret)
+}
+
+#' @export
+channels_se <- function(left_area, ...) {
+  return(left_area)
 }
