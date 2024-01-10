ccf1fe77b8767e915d94c5bdde4fb0083882662b
Ed Baker <edwbaker@gmail.com>
2024-01-10 21:26:48 +0000

    Documentation

--- R/fcis.R
+++ R/fcis.R
@@ -1,17 +1,21 @@
-scaleRGB <- function(vector, col) {
+scaleRGB <- function(vector, no.diff=255) {
+  if (length(unique(vector)) == 1) {
+    return(rep(no.diff, length(vector)))
+  }
   vector <- as.numeric(vector)
   vector <- (255 / (max(vector) - min(vector))) * (vector - min(vector))
+  vector[is.na(vector)] <- 0
   return(as.integer(vector))
 }
 
 #' Map three vectors to RGB
 #'
-#' Maps three vectors of equal length to RGB for use in flase colour index
+#' Maps three vectors of equal length to RGB for use in false-colour index
 #' spectrograms
 #' @param red The red channel vector
 #' @param green The green channel vector
 #' @param blue The blue channel vector
-#' @return A vecotr of RGB values
+#' @return A vector of RGB values
 #' @export
 map2RGB <- function(red, green, blue) {
   red <- scaleRGB(red)
