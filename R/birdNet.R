5118f440434a62912407da6661bca53cbc7f315e
Ed Baker <edwbaker@gmail.com>
2024-02-02 17:26:32 +0000

    Correct filename in readBirdNet

--- R/birdNet.R
+++ R/birdNet.R
@@ -13,27 +13,21 @@ readBirdNet <- function(file, filename_parsing="none") {
     cn <- colnames(ret)
     ret <- cbind(fn, ret)
     colnames(ret) <- c("filename", cn)
+    parts <- strsplit(file, '/')
+    filename <- parts[[1]][[length(parts[[1]])]]
     ret$`Begin.Time..s.` <- as.numeric(ret$`Begin.Time..s.`)
     ret$`End.Time..s.` <- as.numeric(ret$`End.Time..s.`)
     if (filename_parsing == "audiomoth") {
-      parts <- strsplit(file, '/')
-      filename <- parts[[1]][[length(parts[[1]])]]
-
       fn_parts <- strsplit(filename, "_")[[1]]
       start <- as.POSIXct(paste(fn_parts[[1]], fn_parts[[2]]), format="%Y%m%d %H%M%OS")
-
       starts <- start + ret$`Begin.Time..s.`
       ends <- start + ret$`End.Time..s.`
       cn <-colnames(ret)
       ret <- cbind(ret, starts, ends)
       colnames(ret) <- c(cn, "Start", "End")
     } else if (filename_parsing == "timestamp") {
-      parts <- strsplit(file, '/')
-      filename <- parts[[1]][[length(parts[[1]])]]
-
       fn_parts <- strsplit(filename, "\\.")[[1]]
       start <- as.POSIXct(as.numeric(fn_parts[[1]]), origin="1970-01-01")
-
       starts <- start + ret$`Begin.Time..s.`
       ends <- start + ret$`End.Time..s.`
       cn <-colnames(ret)
