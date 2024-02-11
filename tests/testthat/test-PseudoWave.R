83b4e134d1454192021d6e69bab37f1727a8350a
Ed Baker <edwbaker@gmail.com>
2024-02-11 23:54:47 +0000

    depseudoWave tests

--- tests/testthat/test-PseudoWave.R
+++ tests/testthat/test-PseudoWave.R
@@ -9,14 +9,35 @@ test_that("PseudoWave creation", {
 })
 
 test_that("depseduoWave tests", {
+  # Generate mono and stero sine waves
   m <- tuneR::sine(440, duration=44100, samp.rate=44100)
   s <- tuneR::sine(440, duration=44100, samp.rate=44100, stereo=TRUE)
+
+  # Generate a white noise PseudoWave
   pw <- pseudoWave("noise", "white")
+
+  # Add PseudoWave to sine
   mpw <- m + pw
   spw <- s + pw
+
+  # Should return a Wave object
   expect_true(inherits(mpw, "Wave"))
   expect_true(inherits(spw, "Wave"))
   expect_equal(length(mpw@left), 44100)
   expect_equal(length(spw@left), 44100)
   expect_equal(length(spw@right), 44100)
+
+  # Generate mono and stereo silence
+  mq <- tuneR::silence(44100, samp.rate=44100)
+  sq <- tuneR::silence(44100, samp.rate=44100, stereo=TRUE)
+
+  # Multiply PseudoWave by silence
+  mqw <- mq * pw
+  sqw <- sq * pw
+
+  # Should return a silent Wave object
+  expect_true(inherits(mqw, "Wave"))
+  expect_true(inherits(sqw, "Wave"))
+  expect_equal(sum(mqw@left), 0)
+  expect_equal(sum(sqw@right), 0)
 })
