61bcd68458f4e394f8d2042050a2abf4eb44008e
Ed Baker <edwbaker@gmail.com>
2024-02-12 13:39:23 +0000

    Refactor TimeRegion for next release

--- R/TimeRegion.R
+++ R/TimeRegion.R
@@ -17,7 +17,41 @@ setClass(
   )
 )
 
-seconds <- function(from, to) {
+#' Specify a region with a file to analyse
+#'
+#' Specifies a time-bounded region to analyse.
+#' @param unit Unit of time (one of samples, seconds, minutes, hours)
+#' @param from Start time
+#' @param to End time
+#' @return A TimeRegion object.
+#' @export
+region <- function(unit, from=0, to=Inf) {
+  if (!unit %in% c("samples", "seconds", "minutes", "hours")) {
+    stop("Unit must be one of samples, seconds, minutes, hours")
+  }
+  if (unit == "samples") {
+    return(.samples(from, to))
+  }
+  if (unit == "seconds") {
+    return(.seconds(from, to))
+  }
+  if (unit == "minutes") {
+    return(.minutes(from, to))
+  }
+  if (unit == "hours") {
+    return(.hours(from, to))
+  }
+}
+
+.samples <- function(from, to) {
+  t <- new("TimeRegion")
+  t@from <- from
+  t@to <- to
+  t@unit <- "samples"
+  return(t)
+}
+
+.seconds <- function(from, to) {
   t <- new("TimeRegion")
   t@from <- from
   t@to <- to
@@ -25,7 +59,7 @@ seconds <- function(from, to) {
   return(t)
 }
 
-minutes <- function(from, to) {
+.minutes <- function(from, to) {
   t <- new("TimeRegion")
   t@from <- from
   t@to <- to
@@ -33,7 +67,7 @@ minutes <- function(from, to) {
   return(t)
 }
 
-hours <- function(from, to) {
+.hours <- function(from, to) {
   t <- new("TimeRegion")
   t@from <- from
   t@to <- to
@@ -41,7 +75,7 @@ hours <- function(from, to) {
   return(t)
 }
 
-timeRegion2samples <- function(t, samp.rate) {
+.timeRegion2samples <- function(t, samp.rate) {
   if (t@unit == "samples") {
     return(c(t@from,t@to))
   }
@@ -61,7 +95,10 @@ timeRegion2samples <- function(t, samp.rate) {
 #' @param i TimeRegion object
 setMethod("[", signature(x = "Wave", i = "TimeRegion"), function(x,i){
   if (inherits(i,"TimeRegion")) {
-    tr <- timeRegion2samples(i, x@samp.rate)
+    tr <- .timeRegion2samples(i, x@samp.rate)
+    if (is.infinite(tr[2])) {
+      tr[2] <- length(x@left)
+    }
     x@left <- x@left[tr[1]:tr[2]]
     if (x@stereo) {
       x@right <- x@right[tr[1]:tr[2]]
--- tests/testthat/test-TimeRegion.R
+++ tests/testthat/test-TimeRegion.R
@@ -1,33 +1,52 @@
+test_that("Reject incorrect unit", {
+  expect_error(region("monkeys", 0, 10), "Unit must be one of samples, seconds, minutes, hours")
+})
+
+test_that("TimeRegion with samples", {
+  tr <- region("samples", 0, 10)
+  expect_true(inherits(tr, "TimeRegion"))
+  expect_equal(tr@from, 0)
+  expect_equal(tr@to, 10)
+  expect_equal(tr@unit, "samples")
+  expect_equal(.timeRegion2samples(tr, 44100), c(0,10))
+})
+
 test_that("TimeRegion with seconds", {
-  tr <- seconds(0,10)
+  tr <- region("seconds", 0, 10)
   expect_true(inherits(tr, "TimeRegion"))
   expect_equal(tr@from, 0)
   expect_equal(tr@to, 10)
   expect_equal(tr@unit, "seconds")
-  expect_equal(timeRegion2samples(tr, 44100), c(1,44100*10))
+  expect_equal(.timeRegion2samples(tr, 44100), c(1,44100*10))
 })
 
 test_that("TimeRegion with minutes", {
-  tr <- minutes(0,1)
+  tr <- region("minutes", 0, 1)
   expect_true(inherits(tr, "TimeRegion"))
   expect_equal(tr@from, 0)
   expect_equal(tr@to, 1)
   expect_equal(tr@unit, "minutes")
-  expect_equal(timeRegion2samples(tr, 44100), c(1,44100*60))
+  expect_equal(.timeRegion2samples(tr, 44100), c(1,44100*60))
 })
 
 test_that("TimeRegion with hours", {
-  tr <- hours(0,1)
+  tr <- region("hours", 0, 1)
   expect_true(inherits(tr, "TimeRegion"))
   expect_equal(tr@from, 0)
   expect_equal(tr@to, 1)
   expect_equal(tr@unit, "hours")
-  expect_equal(timeRegion2samples(tr, 44100), c(1,44100*60*60))
+  expect_equal(.timeRegion2samples(tr, 44100), c(1,44100*60*60))
 })
 
 test_that("Subsetting Wave by TimeRegion works", {
   w <- tuneR::sawtooth(440, duration=2*44100, samp.rate=44100, stereo=TRUE)
-  w <- w[seconds(1,2)]
+  w <- w[.seconds(1,2)]
   expect_equal(length(w@left), 44101)
   expect_equal(length(w@right), 44101)
+})
+
+test_that("0 and Inf are handled correctly", {
+  w <- tuneR::sawtooth(440, duration=2*44100, samp.rate=44100, stereo=TRUE)
+  w <- w[.seconds(0,Inf)]
+  expect_equal(length(w@left), 88200)
 })
