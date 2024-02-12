975b212a7524480bdaac6e0a881d44aa5356f358
Ed Baker <edwbaker@gmail.com>
2024-02-12 19:32:35 +0000

    More comprehensive frequency tests.

--- tests/testthat/test-frequency.R
+++ tests/testthat/test-frequency.R
@@ -4,8 +4,12 @@ test_that("default to air with good known value", {
 
 test_that("naturalFrequency gives expected output", {
   expect_lt(naturalFrequency(1/2, 1/pi^2, 4) - 0.30775845, 0.001)
+  expect_gt(naturalFrequency(1/2, 1/pi^2, 4) - 0.30775845, -0.001)
+  expect_lt(naturalFrequency(L=20,R=0.5) - 0.002950812, 0.001)
+  expect_gt(naturalFrequency(L=20,R=0.5) - 0.002950812, -0.001)
 })
 
 test_that("resonantFrequency gives expected output", {
   expect_lt(resonantFrequency(1,1) - 0.15915494, 0.001)
+  expect_gt(resonantFrequency(1,1) - 0.15915494, -0.001)
 })
