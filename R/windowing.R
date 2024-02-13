927ec5bdaf8b03ae2565323bcf0bf2c99ab6b160
Ed Baker <edwbaker@gmail.com>
2024-02-13 17:40:12 +0000

    Initial windowing() tests

--- R/windowing.R
+++ R/windowing.R
@@ -2,17 +2,21 @@
 #'
 #' Separates a Wave object into windows of a defined length and runs a function
 #' on the window section. Windows may overlap, and the function can make use of
-#' 'parallel' package for multicore processing. It will also show a progress bar
+#' 'parallel' package for multi-core processing. It will also show a progress bar
 #' if the 'pbapply' package is installed.
 #'
-#' @param wave A Wave object or filename. Using filenames may save loading an entire large file into memory.
+#' @param wave A Wave object or filename. Using filenames may save loading an
+#'   entire large file into memory.
 #' @param window.length The length of the analysis window (in samples).
-#' @param window.overlap The overlap between successive windows (in samples).
-#' @param bind.wave If TRUE and FUN returns wave objects, then these are combined into a single object
+#' @param window.overlap The overlap between successive windows (in samples), a
+#'   negative value will result in a gap between windows.
+#' @param bind.wave If TRUE and FUN returns wave objects, then these are
+#'   combined into a single object
 #' @param FUN FUN to be applied to windows.
 #' @param ... Additional parameters to FUN
-#' @param complete.windows If TRUE (default) the final window will not be processed unless it has a length equal to window.length.
-#' @param cluster A cluster form the 'parallel' package for multicore computation
+#' @param complete.windows If TRUE (default) the final window will not be
+#'   processed unless it has a length equal to window.length.
+#' @param cluster A cluster form the 'parallel' package for multi-core computation.
 #' @keywords wave
 #' @export
 #' @examples
@@ -33,7 +37,7 @@ windowing <- function(
       stop("av package must be installed to use windowing with filename.")
     }
     FUN2 <- function(start, wave, window.length, ...){
-      wave <- readAudio(wave, from=start,to=start+window.length)
+      wave <- readAudio(wave, from=start,to=start+window.length,units="samples")
       return(FUN(wave, start, window.length, ...))
     }
     info <- av::av_media_info(wave)
@@ -46,15 +50,15 @@ windowing <- function(
       return(FUN(section, start, window.length, ...))
     }
   }
-  if (complete.windows) {
-    n.windows <- ceiling(n.samples / (window.length - window.overlap))
-  } else {
-    n.windows <- floor(n.samples / (window.length - window.overlap))
-  }
+  n.windows <- ceiling(n.samples / (window.length - window.overlap))
+
   starts <- c(
     1,
     1:(n.windows-1) * (window.length - window.overlap) +1
   )
+  if (complete.windows) {
+    starts <- starts[which(starts <= n.samples - window.length + 1)]
+  }
 
   if (is.null(cluster)){
     if (package.installed("pbapply", askInstall = FALSE)) {
---
+++ tests/testthat/test-windowing.R
@@ -0,0 +1,51 @@
+test_that("test windowing using filename", {
+  f <- system.file("extdata", "AUDIOMOTH.WAV", package="sonicscrewdriver")
+  f1 <- function(start, wave, window.length) {
+    return(1)
+  }
+  ws <- windowing(f, window.length=10000, FUN=f1)
+  expect_equal(length(ws), 24)
+  expect_equal(sum(as.numeric(ws)), 24)
+
+  f2 <- function(start, wave, window.length) {
+    return(tuneR::silence(1))
+  }
+  ws <- windowing(f, window.length=10000, FUN=f2, bind.wave=TRUE)
+  expect_silent(validateIsWave(ws))
+  expect_equal(length(ws@left), 24)
+})
+
+test_that("test windowing using Wave object", {
+  f <- noise("white", duration=48000*5, samp.rate=48000)
+  f1 <- function(start, wave, window.length) {
+    return(1)
+  }
+  ws <- windowing(f, window.length=10000, FUN=f1)
+  expect_equal(length(ws), 24)
+  expect_equal(sum(as.numeric(ws)), 24)
+
+  f2 <- function(start, wave, window.length) {
+    return(tuneR::silence(1))
+  }
+  ws <- windowing(f, window.length=10000, FUN=f2, bind.wave=TRUE)
+  expect_silent(validateIsWave(ws))
+  expect_equal(length(ws@left), 24)
+})
+
+test_that("Overlap gives expected results", {
+  f <- noise("white", duration=2000, samp.rate=48000)
+  f1 <- function(start, wave, window.length) {
+    return(1)
+  }
+  ws <- windowing(f, window.length=500, window.overlap=0, FUN=f1)
+  expect_equal(length(ws), 4)
+
+  ws <- windowing(f, window.length=500, window.overlap=-500, FUN=f1)
+  expect_equal(length(ws), 2)
+
+  ws <- windowing(f, window.length=500, window.overlap=250, complete.windows=T, FUN=f1)
+  expect_equal(length(ws), 7)
+
+  ws <- windowing(f, window.length=500, window.overlap=250, complete.windows=F, FUN=f1)
+  expect_equal(length(ws), 8)
+})
