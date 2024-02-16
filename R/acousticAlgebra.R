98c7991ad30a2ccf805c4d90a63b7476c3b92207
Ed Baker <edwbaker@gmail.com>
2024-02-16 14:57:46 +0000

    concat noClick method

--- R/acousticAlgebra.R
+++ R/acousticAlgebra.R
@@ -1,10 +1,14 @@
 .concat <- function(object, ..., method) {
-  if (!method %in% c("bind")) {
+  if (!method %in% c("bind", "noClick")) {
     stop("Unknown concatenation method.")
   }
   objectTagged <- .isTagged(object)
-  if (all(sapply(list(object, ...), inherits, c("Wave")))) {
-    object2 <- tuneR::bind(untagWave(object), untagWave(...))
+  if (all(sapply(list(object, ...), inherits, "Wave"))) {
+    allobjects <- untagWave(c(list(object), ...))
+    if (method=="noClick") {
+      allobjects <- .noClick(allobjects)
+    }
+    object2 <- do.call(tuneR::bind, allobjects)
     if (objectTagged) {
       object@left <- object2@left
       object@right <- object2@right
@@ -13,8 +17,12 @@
       return(object2)
     }
   }
-  if (all(sapply(list(object, ...), inherits, c("WaveMC")))) {
-    object2 <- tuneR::bind(untagWave(object), untagWave(...))
+  if (all(sapply(list(object, ...), inherits, "WaveMC"))) {
+    allobjects <- untagWave(c(list(object), list(...)))
+    if (method=="noClick") {
+      allobjects <- .noClick(allobjects)
+    }
+    object2 <- do.call(tuneR::bind, allobjects)
     if (objectTagged) {
       object@`.Data` <- object2@`.Data`
       rm(object2)
@@ -25,21 +33,40 @@
 
   # If tagged then get slots of objects concatenated
   if (objectTagged) {
-    object <- addProcess(object, "concat", .getTags(list(...)))
+    object <- addProcess(object, paste("concat", method, sep="-"), .getTags(list(...)))
   }
 
   return(object)
+}
+
+.noClick <- function(objects) {
+  if (!is(objects, "list")) {
+    stop("Input must be a list.")
+  }
+  if (length(objects) == 1) {
+    return(objects)
+  }
+  objects[[1]] <- tuneR::prepComb(objects[[1]], where="end")
+  objects[[length(objects)]] <- tuneR::prepComb(objects[[length(objects)]], where="start")
+  if (length(objects) == 2) {
+    return(objects)
+  }
+  objects[2:(length(objects)-1)]<- lapply(objects[2:(length(objects)-1)], tuneR::prepComb, where="both")
+  return(objects)
 }
 
 #' Concatenate two or more Wave-like objects.
 #'
 #' The `concat()` method is a more flexible version of the `bind()` method
 #' from `tuneR` package, that allows specifying more advanced types of
-#' concatenation.
+#' concatenation. Setting `method` to "noClick" will remove any click between
+#' Wave objects caused by sudden jumps in amplitude by applying `tuneR::prepComb()`
+#' appropriately with default value of zero (this is only effective for the left
+#' channel or stereo or multi-channel recordings).
 #'
 #' @param object A Wave like object.
 #' @param ... Wave like objects to concatenate to object.
-#' @param method One of "bind".
+#' @param method One of "bind", "noClick". Default is "bind".
 #' @return A concatenated Wave like object, with type of `object`.
 #' @docType methods
 #' @rdname concat-methods
--- tests/testthat/test-acousticAlgebra.R
+++ tests/testthat/test-acousticAlgebra.R
@@ -15,6 +15,18 @@ test_that("bind method on Wave objects works", {
   expect_equal(length(w3@right), length(w1@right) + length(w2@right))
 })
 
+test_that("noClick method on Wave objects works", {
+  w1 <- sine(10.3, samp.rate=450)
+
+  w2 <- concat(w1, w1, method="noClick")
+  expect_true(is(w2, "Wave"))
+  expect_lt(length(w2@left), length(w1@left)*2)
+
+  w3 <- concat(w1, w1, w1, method="noClick")
+  expect_true(is(w3, "Wave"))
+  expect_lt(length(w3@left), length(w1@left)*3)
+})
+
 test_that("bind method on WaveMC objects works", {
   w1 <- sine(440, samp.rate=44100)
   w2 <- sine(440, samp.rate=44100)
@@ -24,6 +36,18 @@ test_that("bind method on WaveMC objects works", {
   w3 <- concat(w1, w2, method="bind")
   expect_true(is(w3, "WaveMC"))
   expect_equal(nrow(w3@.Data), nrow(w1@.Data) + nrow(w2@.Data))
+})
+
+test_that("noClick method on WaveMC objects works", {
+  w1 <- tuneR::WaveMC(sine(10.3, samp.rate=450))
+
+  w2 <- concat(w1, w1, method="noClick")
+  expect_true(is(w2, "WaveMC"))
+  expect_lt(nrow(w2@.Data), nrow(w1@.Data)*2)
+
+  w3 <- concat(w1, w1, w1, method="noClick")
+  expect_true(is(w3, "WaveMC"))
+  expect_lt(nrow(w3@.Data), nrow(w1@.Data)*3)
 })
 
 test_that("bind method on TaggedWave objects works", {
@@ -37,8 +61,21 @@ test_that("bind method on TaggedWave objects works", {
   expect_true(is(w3, "TaggedWave"))
   expect_equal(length(w3@left), length(w1@left) + length(w2@left))
   expect_equal(length(w3@right), length(w1@right) + length(w2@right))
-  expect_equal(w3@processing[[1]]$process, "concat")
+  expect_equal(w3@processing[[1]]$process, "concat-bind")
   expect_equal(w3@processing[[1]]$output[[1]]$metadata$test, "value")
+})
+
+test_that("noClick method on TaggedWave objects works", {
+  w1 <- sine(10.3, samp.rate=450)
+  w1 <- tagWave(w1)
+
+  w2 <- concat(w1, w1, method="noClick")
+  expect_true(is(w2, "TaggedWave"))
+  expect_lt(length(w2@left), length(w1@left)*2)
+
+  w3 <- concat(w1, w1, w1, method="noClick")
+  expect_true(is(w3, "TaggedWave"))
+  expect_lt(length(w3@left), length(w1@left)*3)
 })
 
 test_that("bind method on TaggedWaveMC objects works", {
@@ -53,6 +90,19 @@ test_that("bind method on TaggedWaveMC objects works", {
   w3 <- concat(w1, w2, method="bind")
   expect_true(is(w3, "TaggedWaveMC"))
   expect_equal(nrow(w3@.Data), nrow(w1@.Data) + nrow(w2@.Data))
-  expect_equal(w3@processing[[1]]$process, "concat")
+  expect_equal(w3@processing[[1]]$process, "concat-bind")
   expect_equal(w3@processing[[1]]$output[[1]]$metadata$test, "value")
+})
+
+test_that("noClick method on TaggedWaveMC objects works", {
+  w1 <- tuneR::WaveMC(sine(10.3, samp.rate=450))
+  w1 <- tagWave(w1)
+
+  w2 <- concat(w1, w1, method="noClick")
+  expect_true(is(w2, "TaggedWaveMC"))
+  expect_lt(nrow(w2@.Data), nrow(w1@.Data)*2)
+
+  w3 <- concat(w1, w1, w1, method="noClick")
+  expect_true(is(w3, "TaggedWaveMC"))
+  expect_lt(nrow(w3@.Data), nrow(w1@.Data)*3)
 })
