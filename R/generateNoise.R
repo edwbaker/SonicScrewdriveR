e7e97062f65c6535f520b23b4099f0e48a7059c4
Ed Baker <edwbaker@gmail.com>
2024-02-17 13:08:17 +0000

    generateNoise() for Wave objects

--- R/generateNoise.R
+++ R/generateNoise.R
@@ -6,9 +6,11 @@
 #' @param wave Object to add noise to (`Wave`, `WaveMC`, or Tagged versions), or
 #'   a list of such objects.
 #' @param noise Vector of noise to add (unif, gaussian, white, pink, power, red)
-#' @param noiseAdd If TRUE all noise sources are added to wave. If FALSE
+#' @param noise.add If TRUE all noise sources are added to wave. If FALSE
 #'   separate outputs are created for each noise source.
-#' @param noiseRatio Ratio of maximum noise amplitude to the maximum amplitude in wave.
+#' @param noise.ratio Ratio of maximum noise amplitude to the maximum amplitude in wave.
+#' @param noise.ref Reference maximum for noise.ratio. If "max" then the maximum
+#'   amplitude, if "file" then the maximum amplitude of wave.
 #' @param output TODO: Is this implemented?
 #' @return A list of Wave objects with the required noise added.
 #' @export
@@ -16,15 +18,67 @@
 generateNoise <- function(
   wave,
   noise = c("white"),
-  noiseAdd = FALSE,
-  noiseRatio=0.5,
+  noise.add = FALSE,
+  noise.ratio=0.5,
+  noise.ref="file",
   output = "list"
 ){
-  if (inherits(wave, c("Wave", "WaveMC"))) {
+  if (inherits(wave, c("Wave"))) {
+    if (length(noise) == 1) {
+      n <- tuneR::noise(
+        noise,
+        duration=length(wave@left),
+        samp.rate=wave@samp.rate,
+        bit=wave@bit,
+        stereo=wave@stereo
+      )
+    } else {
+      n <- tuneR::silence(duration=length(wave@left), samp.rate=wave@samp.rate, bit=wave@bit, stereo=wave@stereo)
+      for (i in 1:length(noise)) {
+        nw <- tuneR::noise(noise[i], duration=length(wave@left), samp.rate=wave@samp.rate, bit=wave@bit)
+        n@left <- n@left + nw@left
+        if (wave@stereo) {
+          n@right <- n@right + nw@left
+        }
+      }
+    }
+    if (noise.ref=="file") {
+      noise.max <- max(abs(c(wave@left, wave@right)))
+    } else {
+      noise.max <- (2^n@bit/2)-1
+    }
+    noise.level <- noise.max
+    # Scale n by noise.level
+    n@left <- n@left * noise.level / max(n@left)
+    if (wave@stereo) {
+      n@right <- n@right * noise.level / max(n@right)
+    }
+
+    noise.frac = 1 / (1/noise.ratio - 1)
+    wave@left <- (wave@left + noise.frac * n@left) / (1 + noise.frac)
+    if (wave@stereo) {
+      wave@right <- (wave@right + noise.frac * n@right) / (1 + noise.frac)
+    }
+    if (.isTagged(wave)) {
+      wave <- addProcess(
+        wave,
+        "generateNoise",
+        list(
+          "noise" = noise,
+          "noise.add" = noise.add,
+          "noise.ratio" = noise.ratio,
+          "noise.ref" = noise.ref
+        )
+      )
+    }
+    return(wave)
+  }
+  if (inherits(wave, c("WaveMC"))) {
+    #ToDo
     return(wave)
   }
   if (all(sapply(wave, inherits, c("Wave", "WaveMC")))) {
-    return(lapply(wave, generateNoise, noise, noiseAdd, noiseRatio, output))
+    return(lapply(wave, generateNoise, noise, noise.add, noise.ratio, noise.ref, output))
   }
   stop("wave must be a Wave like object, or a list of such objects.")
 }
--- tests/testthat/test-generateNoise.R
+++ tests/testthat/test-generateNoise.R
@@ -3,31 +3,54 @@ test_that("generateNoise rejects unknown input to wave", {
     tuneR::sine(440, duration=44100, samp.rate=44100),
     tuneR::WaveMC(tuneR::sine(440, duration=44100, samp.rate=44100))
   )
-  expect_silent(generateNoise(w, "white", FALSE, 0.5, "list"))
+  expect_silent(generateNoise(w, "white", FALSE, 0.5, output="list"))
 
   w <- tagWave(w)
-  expect_silent(generateNoise(w, "white", FALSE, 0.5, "list"))
+  expect_silent(generateNoise(w, "white", FALSE, 0.5, output="list"))
 
   w <- list(
     tuneR::sine(440, duration=44100, samp.rate=44100),
     tuneR::WaveMC(tuneR::sine(440, duration=44100, samp.rate=44100)),
     "koi carp"
   )
-  expect_error(generateNoise(w, "white", FALSE, 0.5, "list"), "wave must be a Wave like object, or a list of such objects.")
+  expect_error(generateNoise(w, "white", FALSE, 0.5, output="list"), "wave must be a Wave like object, or a list of such objects.")
 })
 
 test_that("generateNoise outputs somethign Wave-like", {
   w <- tuneR::sine(440, duration=44100, samp.rate=44100)
-  expect_s4_class(generateNoise(w, "white", FALSE, 0.5, "list"), "Wave")
+  expect_s4_class(generateNoise(w, "white", FALSE, 0.5, output="list"), "Wave")
 
   w <- tuneR::WaveMC(tuneR::sine(440, duration=44100, samp.rate=44100))
-  expect_s4_class(generateNoise(w, "white", FALSE, 0.5, "list"), "WaveMC")
+  expect_s4_class(generateNoise(w, "white", FALSE, 0.5, output="list"), "WaveMC")
 
   w <- list(
     tuneR::sine(440, duration=44100, samp.rate=44100),
     tuneR::WaveMC(tuneR::sine(440, duration=44100, samp.rate=44100))
   )
-  expect_type(generateNoise(w, "white", FALSE, 0.5, "list"), "list")
-  expect_s4_class(generateNoise(w, "white", FALSE, 0.5, "list")[[1]], "Wave")
-  expect_s4_class(generateNoise(w, "white", FALSE, 0.5, "list")[[2]], "WaveMC")
+  expect_type(generateNoise(w, "white", FALSE, 0.5, output="list"), "list")
+  expect_s4_class(generateNoise(w, "white", FALSE, 0.5, output="list")[[1]], "Wave")
+  expect_s4_class(generateNoise(w, "white", FALSE, 0.5, output="list")[[2]], "WaveMC")
+})
+
+test_that("noise amplitude is correct on Wave", {
+  w <- pulse("dirac", leading=0)
+  n <- generateNoise(w, "white", FALSE, 0.25, output="list")
+
+  expect_equal(length(w), length(n))
+  expect_equal(which(n@left > max(abs(n@left))/2), 1)
+})
+
+test_that("noise amplitude is correct on TaggedWave", {
+  w <- pulse("dirac", leading=0, output="TaggedWave")
+  n <- generateNoise(w, "white", FALSE, 0.25, output="list")
+  expect_equal(length(w@left), length(n@left))
+  expect_equal(which(n@left > max(abs(n@left))/2), 1)
+
+  w <- pulse("dirac", leading=0, output="TaggedWave", stereo=TRUE)
+  n <- generateNoise(w, "white", FALSE, 0.25, output="list")
+  expect_equal(length(w@left), length(n@right))
+  expect_equal(length(w@left), length(n@left))
+  expect_equal(length(w@right), length(n@right))
+  expect_equal(which(n@left > max(abs(n@left))/2), 1)
+  expect_equal(which(n@right > max(abs(n@left))/2), 1)
 })
