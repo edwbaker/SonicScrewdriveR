e54aeb4aac8c01752e85e37507ff0b3cc3d36709
Ed Baker <edwbaker@gmail.com>
2024-02-19 22:06:29 +0000

    parseFilename() tests

--- tests/testthat/test-parseFilename.R
+++ tests/testthat/test-parseFilename.R
@@ -3,27 +3,30 @@ test_that(".detectFormat() correctly identifies formats", {
     .detectFormat("5E90A4D4.wav"),
     "AudioMoth HEX"
   )
+  expect_null(
+    .detectFormat("__!__.mp3")
+  )
 })
 
 test_that("parseFilename() works as expected", {
-  files <- c(
+  files <- list(
     "5E90A4D4.wav"
   )
-  data <- c(
+  data <- list(
     list(
       filename="5E90A4D4.wav",
       match="AudioMoth HEX",
       datetime=as.POSIXct("2020-04-10 16:54:44 UTC", tz="UTC")
     )
   )
-  expect_equal(parseFilename(files, timezone="UTC"), data)
 
-  data <- c(
-    list(
+  expect_equal(parseFilename(files, timezone="UTC")[[1]]$datetime, data[[1]]$datetime)
+
+  files <- "5E90A4D4.wav"
+  data <- list(
       filename="5E90A4D4.wav",
       match="AudioMoth HEX",
       datetime= as.POSIXct("2020-04-11 01:54:44 JST", tz="Japan")
-    )
   )
   expect_equal(parseFilename(files, timezone="Japan"), data)
 })
