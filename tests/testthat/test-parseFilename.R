1f73e8ae71e5912a927a933dffc5412b4238b83d
Ed Baker <edwbaker@gmail.com>
2024-02-19 13:23:03 +0000

    Remote tests need tz

--- tests/testthat/test-parseFilename.R
+++ tests/testthat/test-parseFilename.R
@@ -13,17 +13,16 @@ test_that("parseFilename() works as expected", {
     list(
       filename="5E90A4D4.wav",
       match="AudioMoth HEX",
-      datetime=as.POSIXct("2020-04-10 17:54:44 BST")
+      datetime=as.POSIXct("2020-04-10 17:54:44 BST", tz="")
     )
   )
   expect_equal(parseFilename(files), data)
 
-  dt <- as.POSIXct("2020-04-11 01:54:44 JST", tz="Japan")
   data <- c(
     list(
       filename="5E90A4D4.wav",
       match="AudioMoth HEX",
-      datetime=dt
+      datetime= as.POSIXct("2020-04-11 01:54:44 JST", tz="Japan")
     )
   )
   expect_equal(parseFilename(files, timezone="Japan"), data)
