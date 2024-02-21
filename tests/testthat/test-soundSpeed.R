010850a58276e8ed07e3078cbc6a9054f3877b24
Ed Baker <edwbaker@gmail.com>
2024-02-21 21:52:37 +0000

    Basic calculations of soundSpeed() tests.

--- tests/testthat/test-soundSpeed.R
+++ tests/testthat/test-soundSpeed.R
@@ -1,3 +1,7 @@
+test_that("error in soundSpeed for unknown medium", {
+  expect_error(soundSpeed(medium="pig semen"))
+})
+
 test_that("default value is for air, and correct", {
   expect_true(is.numeric(soundSpeed()))
   expect_equal(soundSpeed(), 343)
@@ -12,6 +16,10 @@ test_that("specific medium works", {
   expect_equal(soundSpeed(medium="steel"), 5941)
 })
 
-test_that("error in soundSpeed_vague for unknown medium", {
-  expect_error(soundSpeed(medium="pig semen"))
+test_that("frequency and wavelength calculation works", {
+  expect_equal(soundSpeed(f=330, wl=2), 660)
+})
+
+test_that("bulk modulus and density calculation works", {
+  expect_equal(soundSpeed(bulkModulus =2.02e5, density=2), sqrt(1.01e5))
 })
