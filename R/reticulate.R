3f6ff1467d22dabc504ac47b7df253dc3a1413b2
Ed Baker <edwbaker@gmail.com>
2024-07-21 15:50:27 +0100

    Wave to python conversion

--- R/reticulate.R
+++ R/reticulate.R
@@ -1,19 +1,27 @@
-r_to_py.Wave<- function(rx, convert) {
+#' Wave to python conversion
+#'
+#' Helper functions for reticulate to move Wave objects between R and Python
+#' @rdname Wave-python-conversion
+#' @export
+r_to_py.Wave<- function(rx, convert=FALSE) {
   # Check rx is a Wave object
   if (inherits(rx, "Wave")) {
     if (rx@stereo) {
       #ToDo: Handle Stereo
     } else {
       validateIsWave(rx)
-      px <- reticulate::tuple(rx@left, rx@samp.rate, convert=convert)
+      px <- list(as.numeric(unlist(rx@left)), rx@samp.rate)
     }
   } else if (inherits(rx, "WaveMC")) {
     #ToDO: Handle WaveMC
   }
+  #assign("convert", convert, envir = px)
   return(px)
 }
 
-py_to_r.Wave <- function(px) {
+#' @rdname Wave-python-conversion
+#' @export
+py_to_r.Wave <- function(px, convert=FALSE) {
   rx <- Wave(px[[1]], samp.rate = px[[2]])
   return(px)
 }
