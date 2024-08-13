4bbf23c3f5a8943eda6e31db19c4115d07f4e7e3
Ed Baker <edwbaker@gmail.com>
2024-08-13 11:21:31 +0100

    scikit-maad class tests

--- tests/testthat/test-scikit-maad-classes.R
+++ tests/testthat/test-scikit-maad-classes.R
@@ -15,3 +15,10 @@ test_that(".spectrogram_maad_power works as expected", {
   spec <- .spectrogram_maad_power(maad_spectrogram(w))
   expect_s4_class(spec, "spectrogram_maad")
 })
+
+test_that(".spectrogram_maad_dB works as expected", {
+  powers_spec = new("spectrogram_maad", Sxx=matrix(3), mode="power")
+  db_spec = .spectrogram_maad_dB(powers_spec)
+  expect_equal(10 * log10(3), db_spec@Sxx[[1,1]])
+  expect_equal("dB", db_spec@mode)
+})
